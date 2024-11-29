56cc8561d0c72bc35ec324b8a94cbecfb4c71391
RSAHolt <lukejsmart@gmail.com>
2024-11-29 11:33:04 +0200

    push

--- 02 M1-Frontend Web Development/05 - Week 05 - DOM/JavaScript_Part_3_Luke_Smart/Resources/js/main.js
+++ 02 M1-Frontend Web Development/05 - Week 05 - DOM/JavaScript_Part_3_Luke_Smart/Resources/js/main.js
@@ -1,22 +1,40 @@
 let m = document.getElementsByClassName('modal')[0]
 let form = document.getElementById('login-form')
+let p = document.getElementsByClassName('nav-sidebar')[0]
+let butp = document.getElementsByClassName('btn-toggle-nav')[0]
+let show = document.getElementsByClassName('show')[0]
+
 function dismissModal(){
     m.classList.toggle('modal')
+    location.reload()
 }
 
-function validateLogin(){
-
-   
+function validateLogin(e){
+    e.preventDefault();
     form.addEventListener('submit',()=>{
         const {pwd,uname} = form.elements
-        if (pwd == 'password123' && uname == 'admin'){
-            return alert('works')
+        if (pwd.value == 'password123' && uname.value =='admin'){
+            window.location.href = 'index.html';
+            alert('Login Sucessful') 
         }else{
+            console.log('Invalid Credentials');            
             m.style.display = 'block';
-            return
         }
-        // if{
-
-        // } 
+         
     })
 }
+
+form.addEventListener('submit',validateLogin)
+
+function toggleNav(){
+    butp.addEventListener('click',()=>{
+        if(p.style.width == '50px'){
+            p.style.width = '500px'
+            show.style.visibility = 'block'
+        }else{
+            p.style.width = '50px'
+            show.style.visibility = 'hidden'
+        }
+        
+    })
+}   
