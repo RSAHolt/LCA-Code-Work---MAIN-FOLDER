4315455b953ebebedefdf6627ae633f8186b2715
RSAHolt <lukejsmart@gmail.com>
2025-01-31 16:12:28 +0200

    exercise_5

--- 03 M2 Back-End Development and Databases/04_Week 04_Node.Js/express/exercise_5.js
+++ 03 M2 Back-End Development and Databases/04_Week 04_Node.Js/express/exercise_5.js
@@ -1,46 +1,71 @@
-import mysql from 'mysql2/promise'; // /promise allows you to use async and await functions
+import mysql from 'mysql2/promise'
 import express from 'express'
 import {config} from 'dotenv'
 config()
+const PORT = process.env.PORT ||3000
+
 const pool = mysql.createPool({
     hostname: process.env.HOSTNAME,
-    user: process.env.USER,
-    password: process.env.PASSWORD,
+    user: process.env.USER ,
+    password: process.env.PASSWORD ,
     database: process.env.DATABASE
 })
+
 const app = express()
-//3 -- allows json data to be entered in th3e app
 app.use(express.json)
-app.get('/fruit', async (req,res)=>{
-    res.json({fruits: await getFruits()})
+
+
+app.get('/products', async (req,res)=>{
+    res.json({products: await getProducts()})
 })
-app.get('/fruits/:id', async (req,res)=>{
-    res.json({fruits: await getSingleFruit(req.params.id)})
+
+app.post('/products',async(req,res)=>{
+    let {product_code,product_name,product_price,product_quantity} = req.body
+    res.json({products:await insertProduct(product_code,product_name,product_price,product_quantity)})
 })
-//1 path to add new item
-app.post('/fruits', async(req,res)=>{
-    //3 check that data is coming from the frontend --use thunder client
-    console.log(req.body);
-    res.json({
-        fruits: await insertFruit(fruit_name,fruit_colour,seeds)
+
+app.delete('products/:id',async(req,res)=>{
+    res.json({products:deleteProduct(req.params.id)})
+})
+
+app.patch('products/:id',async(req,res)=>{
+    let {product_name,product_price,product_quantity,product_code} = req.body
+    res.json({products:await updateProduct(product_name,product_price,product_quantity,product_code)})
+})
+
+const getProducts = async() => {
+    let [data] = await pool.query('SELECT * FROM products')
+    return data
+}
+
+
+
+const deleteProduct = async(id)=>{
+    await pool.query('DELETE FROM `shopleft`.`products` WHERE (`product_code` = ?)',[id])
+    return await getProducts()
+}
+
+const insertProduct = async(product_code, product_name, product_price, product_quantity) => {
+    await pool.query("INSERT INTO shopleft_database.products (product_code, product_name, product_price, product_quantity) VALUES (?, ?, ?, ?)",[product_code, product_name, product_price, product_quantity])
+    return getProducts()
+}
+
+const updateProduct = async(product_name,product_price,product_quantity,product_code)=>{
+    await pool.query('UPDATE `shopleft`.`products` SET `product_name` = ?, `product_price` = ?, `product_quantity` = ? WHERE (`product_code` = ?)',[product_name,product_price,product_quantity,product_code])
+    return await getProducts()
+}
+
+const getUsers = async () =>{
+    let [data] = await pool.query('SELECT * FROM  users')
+    return data
+}
+app.listen(PORT,()=>{
+    console.log('http://localhost:'+PORT);
     })
-})
-app.listen(3000,()=>{
-    console.log('http://localhost:3000');
-})
-const getFruits = async ()=>{
-    let [data] = await pool.query('SELECT * FROM fruit_list')
-    return data
-}
-const getSingleFruit = async (id)=>{
-    let [data] = await pool.query('SELECT * FROM fruit_list WHERE id = ?',[id])
-    return data
-}
-// 2 make function
-const insertFruit = async()=>{
-    pool.query('INSERT INTO `fruits`.`fruit_list` (`fruit_name`, `fruit_colour`, `seeds`) VALUES (?, ?, ?)', [fruit_name, fruit_colour, seeds])
-    return await getFruits()
-}
-console.log(await getFruits());
-console.log(await getSingleFruit(1));
-console.log('pause');
+
+    
+// deleteProduct('baro1');
+// insertProduct('chick1','Chicken Biltong','10.00','1');
+// updateProduct('cookie1','Cookie','10.00','2','bear1')
+// console.log( await getProducts());
+// console.log( await getUsers());
